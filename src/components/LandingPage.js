2a53a59f5afd329385229ae4eb3c259f2c9c1542
Matt Lovan <matt@moneymail.ai>
2021-05-22 10:53:50 -0400

    check in env, update readme

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,7 +10,6 @@ function LandingPage({}) {
         if (response.status !== 200) {
           throw Error(response.statusText);
         }
-
         return response.json();
       })
       .then((data) => {
@@ -25,13 +24,11 @@ function LandingPage({}) {
   return (
     <div className='container'>
       <h1>Leaderboard</h1>
-      {/* loading spinner */}
-
       {error ? <code>{error}</code> : null}
-
       <table>
         <thead>
           <th>Name</th>
+          <th>Email</th>
           <th>Public Address</th>
           <th>Ra's Earned</th>
           <th>Ra's Spent</th>
@@ -43,6 +40,7 @@ function LandingPage({}) {
               return (
                 <tr>
                   <td>{user.first_name + ' ' + user.last_name}</td>
+                  <td>{user.email}</td>
                   <td>{user.publicAddress}</td>
                   <td>{user.ra_earned}</td>
                   <td>{user.ra_spent}</td>
